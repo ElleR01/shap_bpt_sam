e6daca19cc742902497e6ec19c8e59adbc54cea1
Muhammad Rashid <muhammadrashid8045@gmail.com>
2026-08-06 17:37:46 +0200

    Run experiments on Epito/A100

---
+++ examples/scripts/check_setup.py
@@ -0,0 +1,2 @@
+import shap_bpt as shap_bpt
+print('shap_bpt version:',shap_bpt.__version__)
---
+++ examples/scripts/test_GPU.py
@@ -0,0 +1,3 @@
+import torch
+print('Torch version:', torch.__version__)
+print('Torch location:', torch.__file__)
